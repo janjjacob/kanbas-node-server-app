3c80eab6a0f078c15662ee1bfd54af3f8b5481fb
janjjacob <janjjacob@gmail.com>
2023-12-04 20:34:52 -0500

    added console log

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import session from "express-session";
 import "dotenv/config";
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+console.log(CONNECTION_STRING);
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
